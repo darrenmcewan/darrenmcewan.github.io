cb61b4e7a30b30915a8900165dfdb0c696f40c3f
Darren McEwan <damcewan@cisco.com>
2023-04-13 14:47:19 -0700

    Adding a place-holder

--- clock.js
+++ clock.js
@@ -1,106 +1,45 @@
-$(function(){
+// set these to change interval and video (no error checking yet)
+const startTime = 9;
+const endTime = 14;
+const videoID = "QQNL83fhWJU";
 
-    // Cache some selectors
+// load JS for API
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    var clock = $('#clock'),
-        alarm = clock.find('.alarm'),
-        ampm = clock.find('.ampm');
+let player;
 
-    // Map digits to their names (this will be an array)
-    var digit_to_name = 'zero one two three four five six seven eight nine'.split(' ');
+// when YT API iframe is ready, this is called
+function onYouTubeIframeAPIReady() {
+  // create the player
+  player = new YT.Player("player", {
+    height: "900",
+    width: "640",
+    videoId: videoID,
+    playerVars: {
+      playsinline: 1,
+      mute: 1,
+      autoplay: 1,
+      controls: 0
+    },
+    events: {
+      onReady: (event) => {
+        //start video when it is ready
+        event.target.playVideo();
+        // start looping
+        loop();
+      }
+    }
+  });
+}
 
-    // This object will hold the digit elements
-    var digits = {};
+/* move to start time, sets timer to start loop over at end time
 
-    // Positions for the hours, minutes, and seconds
-    var positions = [
-        'h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2'
-    ];
+zombie functions like setTimout that continually run aren't ideal. In a production site, to be polite, you might want to add more code to shut the video off after a certain amount of time. */
+loop = () => {
+  player.seekTo(startTime, true);
+  setTimeout(loop, (endTime - startTime) * 1000);
+};
 
-    // Generate the digits with the needed markup,
-    // and add them to the clock
-
-    var digit_holder = clock.find('.digits');
-
-    $.each(positions, function(){
-
-        if(this == ':'){
-            digit_holder.append('<div class="dots">');
-        }
-        else{
-
-            var pos = $('<div>');
-
-            for(var i=1; i<8; i++){
-                pos.append('<span class="d' + i + '">');
-            }
-
-            // Set the digits as key:value pairs in the digits object
-            digits[this] = pos;
-
-            // Add the digit elements to the page
-            digit_holder.append(pos);
-        }
-
-    });
-
-    // Add the weekday names
-
-    var weekday_names = 'SUN MON TUE WED THU FRI SAT'.split(' '),
-        weekday_holder = clock.find('.weekdays');
-
-    $.each(weekday_names, function(){
-        weekday_holder.append('<span>' + this + '</span>');
-    });
-
-    var weekdays = clock.find('.weekdays span');
-
-    // Run a timer every second and update the clock
-
-    (function update_time(){
-
-        // Use moment.js to output the current time as a string
-        // hh is for the hours in 12-hour format,
-        // mm - minutes, ss-seconds (all with leading zeroes),
-        // d is for day of week and A is for AM/PM
-
-        var now = moment().format("hhmmssdA");
-
-        digits.h1.attr('class', digit_to_name[now[0]]);
-        digits.h2.attr('class', digit_to_name[now[1]]);
-        digits.m1.attr('class', digit_to_name[now[2]]);
-        digits.m2.attr('class', digit_to_name[now[3]]);
-        digits.s1.attr('class', digit_to_name[now[4]]);
-        digits.s2.attr('class', digit_to_name[now[5]]);
-
-        // The library returns Sunday as the first day of the week.
-        // Stupid, I know. Lets shift all the days one position down, 
-        // and make Sunday last
-
-        var dow = now[6];
-        //dow--;
-
-        // Sunday!
-        //if(dow < 0){
-            // Make it last
-         //   dow = 6;
-        //}
-
-        // Mark the active day of the week
-        weekdays.removeClass('active').eq(dow).addClass('active');
-
-        // Set the am/pm text:
-        ampm.text(now[7]+now[8]);
-
-        // Schedule this function to be run again in 1 sec
-        setTimeout(update_time, 1000);
-
-    })();
-
-    // Switch the theme
-
-    $('a.button').click(function(){
-        clock.toggleClass('light dark');
-    });
-
-});
